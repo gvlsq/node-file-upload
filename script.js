90e92c973a9ebd31c20e79e1d779876fac59251b
gvelasquez <54555927+twelvefifteen@users.noreply.github.com>
2021-11-21 20:36:35 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+/*
+** @TODO:
+** - https://github.com/richardgirges/express-fileupload/issues/148
+** - https://smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
+** - https://stackoverflow.com/questions/64803772/nodejs-fileupload-multer-vs-express-fileupload-which-to-use/64805230#64805230
+*/
