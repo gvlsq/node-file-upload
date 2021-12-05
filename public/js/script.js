a5cfcb2b1d321579847fac8a3d9a11283e68a83d
gvelasquez <54555927+twelvefifteen@users.noreply.github.com>
2021-12-05 17:18:17 -0600

    Functioning upload, and frontend fleshed out to show uploads

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,120 @@
-/*
-** @TODO:
-** - https://github.com/richardgirges/express-fileupload/issues/148
-** - https://smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
-** - https://stackoverflow.com/questions/64803772/nodejs-fileupload-multer-vs-express-fileupload-which-to-use/64805230#64805230
-*/
+const container = document.getElementById("container");
+const uploadArea = document.getElementById("uploadArea");
+["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
+  uploadArea.addEventListener(eventName, (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }, false);
+});
+["dragenter", "dragover"].forEach((eventName) => {
+  uploadArea.addEventListener(eventName, (e) => {
+    uploadArea.classList.add("highlight");
+  }, false);
+});
+["dragleave", "drop"].forEach((eventName) => {
+  uploadArea.addEventListener(eventName, (e) => {
+    uploadArea.classList.remove("highlight");
+  }, false);
+});
+uploadArea.addEventListener("drop", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
+  const dataTransfer = e.dataTransfer;
+  const files = dataTransfer.files;
+
+  const uploadURL = "upload";
+
+  uploadFiles([...files]);
+}, false);
+
+const uploadedFiles = document.getElementById("uploadedFiles");
+console.log(uploadedFiles);
+
+const formatByteCount = (byteCount) => {
+  let iterations = 0;
+  while (byteCount > 1024) {
+    byteCount /= 1024;
+
+    iterations++;
+    if (iterations === 3) break;
+  }
+
+  let result = `${Math.floor(byteCount*100) / 100} `;
+  switch (iterations) {
+    case 0:
+      result += "B";
+      break;
+
+    case 1:
+      result += "KB";
+      break;
+
+    case 2:
+      result += "MB";
+      break;
+
+    default:
+      result += "GB";
+      break;
+  }
+
+  return result;
+}
+
+const truncateFileName = (fileName) => {
+  let result;
+  
+  const maxLength = 26;
+
+  if (fileName.length > maxLength) {
+    result = fileName.substr(0, maxLength - 1) + "..." + fileName.substr(fileName.length - 4, fileName.length);
+  }
+
+  return result;
+}
+
+const uploadFiles = async (files) => {
+  if (!Array.isArray(files)) files = Array.from(files);
+
+  for (const file of files) {
+    let formData = new FormData();
+    formData.append("file", file);
+
+    const response = await fetch("upload", {
+      method: "POST",
+      body: formData
+    });
+    const responseJSON = await response.json();
+
+    const fileData = responseJSON.file;
+    
+    let uploadedFileDiv = document.createElement("div");
+    uploadedFileDiv.classList.add("uploaded-file");
+
+    let documentImage = document.createElement("img");
+    documentImage.src = "img/image.svg";
+    documentImage.classList.add("document-image");
+    uploadedFileDiv.appendChild(documentImage);
+
+    let fileDescriptionDiv = document.createElement("div");
+    fileDescriptionDiv.classList.add("file-description");
+    {
+      let filenameH3 = document.createElement("h3");
+      filenameH3.appendChild(document.createTextNode(truncateFileName(fileData.originalName)));
+      fileDescriptionDiv.appendChild(filenameH3);
+
+      let fileSizeH4 = document.createElement("h4");
+      fileSizeH4.appendChild(document.createTextNode(formatByteCount(fileData.size)));
+      fileDescriptionDiv.appendChild(fileSizeH4);
+    }
+    uploadedFileDiv.appendChild(fileDescriptionDiv);
+
+    let statusImage = document.createElement("img");
+    statusImage.src = "img/link.svg";
+    statusImage.classList.add("link-image");
+    uploadedFileDiv.appendChild(statusImage);
+
+    container.insertBefore(uploadedFileDiv, uploadArea);
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,78 @@
+import fs, {
+  promises as fsPromises
+} from "fs";
 import path from "path";
 
-import cons from "consolidate";
+import dotenv from "dotenv";
 import express, {
   Application,
   Request,
   Response,
   NextFunction
 } from "express";
+import morganBody from "morgan-body";
+import multer from "multer";
+
+dotenv.config();
+
+const upload = multer({
+  dest: "tmp/uploads/"
+});
 
 const app: Application = express();
 
-// Consolidate used here instead of express-handlebars because of a bug related
-// to this: https://stackoverflow.com/a/70010359
-app.engine("handlebars", cons.handlebars);
-app.set("view engine", "handlebars");
-app.set("views", path.resolve(__dirname, "../views"));
+morganBody(app, {
+  logIP: false,
+  logReqUserAgent: false,
+  noColors: true,
+  timezone: "Etc/UTC"
+});
 
 app.use(express.static(path.resolve(__dirname, "../public")));
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello");
+  try {
+    res.sendFile(path.join(__dirname, "../public", "index.html"));
+  } catch (error: any) {
+    next(error);
+  }
+});
+app.post("/upload", upload.single("file"), async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const file = req.file as Express.Multer.File;
+  
+    //
+    // This is where you would send the file to Azure Blob Storage, store it in a
+    // FILESTREAM, et cetera
+    //
+
+    // Uncomment this line to delete the file stored in the temporary disk folder
+    // used by multer
+    // await fsPromises.unlink(file.path);
+    
+    res.status(201).json({
+      success: true,
+      file: {
+        originalName: file.originalname,
+        size: file.size
+      }
+    });
+  } catch (error: any) {
+    next(error);
+  }
+});
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  const formattedMessage = `Exception thrown: ${err.message}`;
+
+  console.error(formattedMessage);
+
+  res.status(500).json({
+    success: false,
+    message: formattedMessage
+  });
 });
 
-app.get("/home", (req: Request, res: Response, next: NextFunction) => {
-  res.render("index");
-});
-
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started in ${process.env.NODE_ENV} on port ${PORT}`);
 });
