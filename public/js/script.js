65e2c58305af8527cf55e6c520260722afcc2039
gvelasquez <54555927+twelvefifteen@users.noreply.github.com>
2021-12-06 16:38:15 -0600

    Massive script.js refactor

--- public/js/script.js
+++ public/js/script.js
@@ -1,82 +1,49 @@
-const container = document.getElementById("container");
-const uploadArea = document.getElementById("uploadArea");
-["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
-  uploadArea.addEventListener(eventName, (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-  }, false);
-});
-["dragenter", "dragover"].forEach((eventName) => {
-  uploadArea.addEventListener(eventName, (e) => {
-    uploadArea.classList.add("highlight");
-  }, false);
-});
-["dragleave", "drop"].forEach((eventName) => {
-  uploadArea.addEventListener(eventName, (e) => {
-    uploadArea.classList.remove("highlight");
-  }, false);
-});
-uploadArea.addEventListener("drop", (e) => {
-  const dataTransfer = e.dataTransfer;
-  const files = dataTransfer.files;
-
-  const uploadURL = "upload";
-
-  uploadFiles([...files]);
-}, false);
-
-const uploadedFiles = document.getElementById("uploadedFiles");
-
-const formatByteCount = (byteCount) => {
-  let iterations = 0;
-  while (byteCount > 1024) {
-    byteCount /= 1024;
-
-    iterations++;
-    if (iterations === 3) break;
+class UI {
+  static init() {
+    const uploadArea = document.querySelector("#uploadArea");
+    ["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
+      uploadArea.addEventListener(eventName, (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+      }, false);
+    });
+    ["dragenter", "dragover"].forEach((eventName) => {
+      uploadArea.addEventListener(eventName, (e) => {
+        uploadArea.classList.add("highlight");
+      }, false);
+    });
+    ["dragleave", "drop"].forEach((eventName) => {
+      uploadArea.addEventListener(eventName, (e) => {
+        uploadArea.classList.remove("highlight");
+      }, false);
+    });
   }
 
-  let result = `${Math.floor(byteCount*100) / 100} `;
-  switch (iterations) {
-    case 0:
-      result += "B";
-      break;
+  static addUploadedFile(fileData) {
+    let fileName = fileData.originalName;
+    if (fileName.length > 26)
+      fileName = fileName.substr(0, 26 - 1) + "..." + fileName.substr(fileName.length - 4, fileName.length);
 
-    case 1:
-      result += "KB";
-      break;
-
-    case 2:
-      result += "MB";
-      break;
-
-    default:
-      result += "GB";
-      break;
+    let div = document.createElement("div");
+    div.classList.add("uploaded-file");
+    div.innerHTML = `<img src="img/image.svg" class="document-image">
+                     <div class="file-description">
+                       <h3>${fileName}</h3>
+                       <h4>${Helper.formatByteCount(fileData.size)}</h4>
+                     </div>
+                     <img src="img/link.svg" class="link-image">`;
+    
+    const container = document.querySelector("#container");
+    const uploadArea = document.querySelector("#uploadArea");
+    
+    container.insertBefore(div, uploadArea);
   }
-
-  return result;
 }
 
-const truncateFileName = (fileName) => {
-  let result = fileName;
-  
-  const maxLength = 26;
-  if (fileName.length > maxLength) {
-    result = fileName.substr(0, maxLength - 1) + "..." + fileName.substr(fileName.length - 4, fileName.length);
-  }
-
-  return result;
-}
-
-const uploadFiles = async (files) => {
-  if (!Array.isArray(files)) files = Array.from(files);
-
-  for (const file of files) {
-    if (file.type.substr(0, 6) != "image/") {
-      alert("Uploads must be images");
-      break;
-    }
+class Uploader {
+  static async uploadFile(file) {
+    if (file.type.substr(0, 6) != "image/")
+      alert("Only images are accepted for upload");
 
     let formData = new FormData();
     formData.append("file", file);
@@ -88,33 +55,59 @@ const uploadFiles = async (files) => {
     const responseJSON = await response.json();
 
     const fileData = responseJSON.file;
-    
-    let uploadedFileDiv = document.createElement("div");
-    uploadedFileDiv.classList.add("uploaded-file");
 
-    let documentImage = document.createElement("img");
-    documentImage.src = "img/image.svg";
-    documentImage.classList.add("document-image");
-    uploadedFileDiv.appendChild(documentImage);
-
-    let fileDescriptionDiv = document.createElement("div");
-    fileDescriptionDiv.classList.add("file-description");
-    {
-      let filenameH3 = document.createElement("h3");
-      filenameH3.appendChild(document.createTextNode(truncateFileName(fileData.originalName)));
-      fileDescriptionDiv.appendChild(filenameH3);
-
-      let fileSizeH4 = document.createElement("h4");
-      fileSizeH4.appendChild(document.createTextNode(formatByteCount(fileData.size)));
-      fileDescriptionDiv.appendChild(fileSizeH4);
-    }
-    uploadedFileDiv.appendChild(fileDescriptionDiv);
-
-    let statusImage = document.createElement("img");
-    statusImage.src = "img/link.svg";
-    statusImage.classList.add("link-image");
-    uploadedFileDiv.appendChild(statusImage);
-
-    container.insertBefore(uploadedFileDiv, uploadArea);
+    return fileData;
   }
 }
+
+class Helper {
+  static formatByteCount(byteCount) {
+    let iterations = 0;
+    while (byteCount > 1024) {
+      byteCount /= 1024;
+
+      iterations++;
+      if (iterations === 3) break;
+    }
+
+    let result = `${Math.floor(byteCount*100) / 100} `;
+    switch (iterations) {
+      case 0:
+        result += "B";
+        break;
+
+      case 1:
+        result += "KB";
+        break;
+
+      case 2:
+        result += "MB";
+        break;
+
+      default:
+        result += "GB";
+        break;
+    }
+
+    return result;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", UI.init);
+document.querySelector("#uploadArea").addEventListener("drop", async (e) => {
+  const dataTransfer = e.dataTransfer;
+  const files = dataTransfer.files;
+  if (files.count < 1) return;
+
+  const fileData = await Uploader.uploadFile(files[0]);
+  UI.addUploadedFile(fileData);
+});
+document.querySelector("#uploadFileInput").addEventListener("change", async (e) => {
+  const files = e.target.files;
+  if (files.length < 1) return;
+
+  const file = files.item(0);
+  
+  const fileData = await Uploader.uploadFile(file);
+  UI.addUploadedFile(fileData);
+});
